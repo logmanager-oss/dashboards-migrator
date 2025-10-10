73d6fc00f460b2338864e47cc6763b8fac18dfc8
solnicki <solnicki@logmanager.pl>
2025-10-10 12:41:48 +0200

    refactor: update flags usage

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Config represents user supplied program input
@@ -17,24 +18,25 @@ type Config struct {
 // LoadAndValidate loads values from user supplied input into Config struct and validates them
 func (c *Config) LoadAndValidate() error {
 	flag.StringVar(&c.InputPath, "i", "", "Path to input file containing LM3 dashboard (Mandatory)")
+	flag.StringVar(&c.OutputPath, "o", "", "Path to output file containing LM4 dashboard (Mandatory)")
+	flag.StringVar(&c.IndexPattern, "ip", "", "Index-pattern string (Default: empty)")
+
+	flag.Parse()
+
 	err := c.validateInputPath(c.InputPath)
 	if err != nil {
 		return fmt.Errorf("validating input path: %w", err)
 	}
 
-	flag.StringVar(&c.OutputPath, "o", "./", "Path to output file containing LM4 dashboard (Default: current path)")
 	err = c.validateOutputPath(c.OutputPath)
 	if err != nil {
 		return fmt.Errorf("validating output path: %w", err)
 	}
 
-	flag.StringVar(&c.IndexPattern, "ip", "", "Index-pattern string (Default: empty)")
 	err = c.validateIndexPattern(c.IndexPattern)
 	if err != nil {
 		return fmt.Errorf("validating index-pattern: %w", err)
 	}
-
-	flag.Parse()
 
 	return nil
 }
@@ -57,6 +59,10 @@ func (c *Config) validateInputPath(flagValue string) error {
 }
 
 func (c *Config) validateOutputPath(flagValue string) error {
+	if flagValue == "" {
+		return fmt.Errorf("path to output file (-o flag) cannot be empty")
+	}
+
 	fileInfo, err := os.Stat(flagValue)
 	if err != nil {
 		// If output path does not exist it's ok - we will create it
@@ -74,7 +80,10 @@ func (c *Config) validateOutputPath(flagValue string) error {
 	return nil
 }
 
-// TODO: add index-pattern validation
-func (c *Config) validateIndexPattern(_ string) error {
+func (c *Config) validateIndexPattern(flagValue string) error {
+	if !strings.HasPrefix(flagValue, "lm-") {
+		return fmt.Errorf("index-pattern must begin with \"lm-\" prefix")
+	}
+
 	return nil
 }
